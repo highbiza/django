f29e3c02a7ac6eb5c1e9c0aca66f020a8e786beb
Joey Jurjens <joey@highbiza.nl>
2026-06-17 10:34:20 +0200

    fix more crap ai

--- tests/user_commands/tests.py
+++ tests/user_commands/tests.py
@@ -177,8 +177,8 @@ class CommandTests(SimpleTestCase):
 
     def test_requires_system_checks_specific(self):
         with mock.patch('django.core.management.base.BaseCommand.check') as mocked_check:
-            management.call_command('specific_system_checks')
-        mocked_check.called_once_with(tags=[Tags.staticfiles, Tags.models])
+            management.call_command('specific_system_checks', skip_checks=False)
+        mocked_check.assert_called_once_with(tags=[Tags.staticfiles, Tags.models])
 
     def test_requires_system_checks_invalid(self):
         class Command(BaseCommand):
